8d16d08fc506250035308d371c35479d39a87857
diamondburned <diamond@arikawa-hi.me>
2024-02-22 06:13:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# prettier-gohtml-nix
+
+Prettier with
+[prettier-plugin-go-template](https://github.com/NiklasPor/prettier-plugin-go-template)
+baked into a Nix package.
+
+## Usage
+
+Import the Flake as usual. The binary is in `packages.${system}.default`.
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+		flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+		npmlock2nix.url = "github:nix-community/npmlock2nix";
+		npmlock2nix.flake = false;
+	};
+
+	outputs =
+		{ self, nixpkgs, flake-utils, flake-compat, npmlock2nix }:
+
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
+					npmlock2nix = super.callPackage "${npmlock2nix}/internal-v2.nix" { };
+				});
+				packageJSON = builtins.fromJSON (builtins.readFile ./package.json);
+				prettierPlugins = packageJSON.prettier.plugins;
+			in
+			{
+				packages = rec {
+					node_modules = pkgs.npmlock2nix.node_modules {
+						src = self;
+						nodejs = pkgs.nodejs;
+					};
+					default =
+						let
+							prettierPluginPaths = map (plugin: "${node_modules}/node_modules/${plugin}/lib/index.js") prettierPlugins;
+							prettierPluginFlags = builtins.concatStringsSep " " (map (x: "--plugin ${x}") prettierPluginPaths);
+						in
+							pkgs.writeShellScriptBin "prettier" ''
+								${node_modules}/bin/prettier ${prettierPluginFlags} "$@"
+							'';
+				};
+				devShell = pkgs.mkShell {
+					packages = with pkgs; [
+						nodejs
+					];
+				};
+			}
+		);
+}
